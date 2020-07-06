2cf6caa2d9741496f13c7ad22624903db72355a1
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2020-07-05 23:20:05 -0700

    Added gravatar and encryption for register rout

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const User = require('../../models/User');
 
@@ -11,18 +13,32 @@ router.post('/register', (req, res) => {
       if (user){
         return res.status(400).json({email: 'Email already exists'});
       } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+        });
+
         const newUser = new User({
           name: req.body.name,
           email: req.body.email,
+          avatar: avatar,
           password: req.body.password
         });
-        newUser.save()
-          .then(user => res.json(user) )
-          .catch(err => console.log(err));
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+            .save()
+            .then(user => res.json(user) )
+            .catch(err => console.log(err));
+          });
+        });
+                 
       }
     })
     .catch(err => console.log(err));
 })
-
-
 module.exports = router;
