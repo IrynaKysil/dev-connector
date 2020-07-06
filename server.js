5d8f341b6b81dae1d93ca00d02a499e6ceb15dc9
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2020-07-05 21:58:26 -0700

    Added user model and user route

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
-const { use } = require('passport');
 const app = express();
+
+//Body parser configuration
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 
 //Db config
 const db = require('./config/keys').mongoURI;
 
 //Connect to mongodb
 mongoose
-.connect(db)
-.then(() => console.log('Mongodb connected'))
-.catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('Mongodb connected'))
+  .catch(err => console.log(err));
 
 //Let's write our first route
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res)=> res.send('Hello'));
 
 //Use routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
-app.use('/api/posts', posts)
+app.use('/api/posts', posts);
 
 const port = 8008;
 app.listen(port, () => console.log(`Server running on port ${port}`));
