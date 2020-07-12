916ce5da3d9edfb07dfd80eb2ed680b8a1c3b555
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2020-07-11 23:40:06 -0700

    Added register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <Navbar />
-        <Landing />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
         <Footer />
         </div>
+        </Router>
     )
   }
 }
