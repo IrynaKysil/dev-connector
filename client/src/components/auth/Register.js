716164d77c8a56a2cea19a08ad107569936b46ab
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2020-07-23 16:22:28 -0700

    Binding form-control values to the local state for register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -10,7 +10,11 @@ class Register extends Component {
       password2: '',
       errors: {}
     }
+    this.onChange = this.onChange.bind(this);
+  }
 
+  onChange(e){
+    this.setState({[e.target.name]: e.target.value})
   }
 
   render() {
@@ -23,20 +27,45 @@ class Register extends Component {
             <p className="lead text-center">Create your DevConnector account</p>
             <form action="create-profile.html">
               <div className="form-group">
-                <input type="text" className="form-control form-control-lg" placeholder="Name" name="name"
+                <input 
+                type="text" 
+                className="form-control form-control-lg" 
+                placeholder="Name" 
+                name="name"
                 value={this.state.name}
                 onChange={this.onChange}
                 required />
               </div>
               <div className="form-group">
-                <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" />
+                <input 
+                type="email" 
+                className="form-control form-control-lg" 
+                placeholder="Email Address" 
+                name="email" 
+                value={this.state.email}
+                onChange={this.onChange}
+                />
                 <small classNameName="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
               </div>
               <div className="form-group">
-                <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" />
+                <input 
+                type="password" 
+                className="form-control form-control-lg" 
+                placeholder="Password" 
+                name="password"
+                value={this.state.password}
+                onChange={this.onChange}
+                 />
               </div>
               <div className="form-group">
-                <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" />
+                <input 
+                type="password" 
+                className="form-control form-control-lg" 
+                placeholder="Confirm Password" 
+                name="password2"
+                value={this.state.password2}
+                onChange={this.onChange}
+                />
               </div>
               <input type="submit" className="btn btn-info btn-block mt-4" />
             </form>
