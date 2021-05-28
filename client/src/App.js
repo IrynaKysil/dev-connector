c0c94649b9f21c5b83af79516e123b40d256add6
IrynaK <31361447+IrynaKysil@users.noreply.github.com>
2021-05-27 22:59:02 -0700

    Redux: create store, create reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Router>
       <div className="App">
         <Navbar />
@@ -19,6 +22,8 @@ class App extends Component {
         <Footer />
         </div>
         </Router>
+        </Provider>
+      
     )
   }
 }
